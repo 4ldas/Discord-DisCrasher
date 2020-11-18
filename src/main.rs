378ead3bc00000c8df6d6e8efeb82ca0d6c13fbd
Aldas <40773946+faith@users.noreply.github.com>
2020-11-18 21:17:51 +0000

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -88,7 +88,7 @@ struct GuildSettings {
     verification_level: Option<u8>
 }
 
-fn change_settings(token: &str, guild_id: &usize, settings: &GuildSettings) -> Result<(), Box<std::error::Error>) {
+fn change_settings(token: &str, guild_id: &usize, settings: &GuildSettings) -> Result<(), Box<std::error::Error>> {
     let req = reqwest::blocking::Client::new()
     .patch(&format!("https://discord.com/api/v6/guilds/{}", guild_id))
     .header("authorization", token)
